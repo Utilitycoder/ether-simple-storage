330d6f8ffb5a109fb2b8c1356eb32db76f92d09d
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-06-14 12:08:54 +0100

    update

--- deploy.js
+++ deploy.js
@@ -1,42 +1,43 @@
-const ethers = require("ethers")
-const fs = require("fs-extra")
+const ethers = require("ethers");
+const fs = require("fs-extra");
 
 async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PASSWORD
+  );
+  wallet = wallet.connect(provider);
 
-    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545")
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-      encryptedJson,
-      process.env.PASSWORD
-    );
-    wallet = wallet.connect(provider);
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
 
-    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
-    const binary = fs.readFileSync(
-        "./SimpleStorage_sol_SimpleStorage.bin",
-        "utf8"
-    )
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  // const contract = await contractFactory.deploy({ gasPrice: 100000000000 })
+  const deploymentReceipt = await contract.deployTransaction.wait(1);
+  console.log(`Contract deployed to ${contract.address}`);
+  // const wallet = new ethers.Wallet("5d825035963f0e06ac55b882019e4eaf4e8ed81db2fb00ad704b0d88384db657", provider)
 
-    const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
-    console.log("Deploying, please wait...")
-    const contract = await contractFactory.deploy()
-    // const contract = await contractFactory.deploy({ gasPrice: 100000000000 })
-    const deploymentReceipt = await contract.deployTransaction.wait(1)
-    console.log(`Contract deployed to ${contract.address}`)
-    // const wallet = new ethers.Wallet("5d825035963f0e06ac55b882019e4eaf4e8ed81db2fb00ad704b0d88384db657", provider)
-
-    let currentFavoriteNumber = await contract.retrieve()
-    console.log(`Current Favorite Number: ${currentFavoriteNumber}`)
-    console.log("Updating favorite number...")
-    let transactionResponse = await contract.store(7)
-    let transactionReceipt = await transactionResponse.wait()
-    currentFavoriteNumber = await contract.retrieve()
-    console.log(`New Favorite Number: ${currentFavoriteNumber}`)
+  let currentFavoriteNumber = await contract.retrieve();
+  console.log(`Current Favorite Number: ${currentFavoriteNumber}`);
+  console.log("Updating favorite number...");
+  let transactionResponse = await contract.store(7);
+  let transactionReceipt = await transactionResponse.wait();
+  currentFavoriteNumber = await contract.retrieve();
+  console.log(`New Favorite Number: ${currentFavoriteNumber}`);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.log(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
